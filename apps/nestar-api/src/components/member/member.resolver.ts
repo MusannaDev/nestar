7a94bc155ab0fba856940c4905e732ddb5a74bd0
MusannaDev <juraevotabek2002@gmail.com>
2025-12-28 02:07:40 +0900

    feat: create member schema model

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,4 +1,37 @@
-import { Resolver } from '@nestjs/graphql';
+import { Mutation, Resolver, Query } from '@nestjs/graphql';
+import { MemberService } from './member.service';
+
 
 @Resolver()
-export class MemberResolver {}
+export class MemberResolver {
+  constructor(private readonly memberService: MemberService) {}
+
+  @Mutation(() => String)
+  public async signup(): Promise<String> {
+    console.log("Mutation: singup")
+    return this.memberService.signup();
+  }
+  @Mutation(() => String)
+  public async login(): Promise<String> {
+    console.log("Mutation: login")
+    return this.memberService.login();
+  }
+
+  @Mutation(() => String)
+  public async updateMember(): Promise<String> {
+    console.log("Mutation: updateMember")
+    return this.memberService.updateMember();
+  }
+
+  @Query(() => String)
+  public async getMember(): Promise<String> {
+    console.log("Mutation: singup")
+    return this.memberService.getMember();
+  }
+
+  @Query(() => String)
+  public async getMemberDetail(): Promise<String> {
+    console.log("Mutation: getMemberDetail")
+    return this.memberService.getMemberDetail();
+  }
+}
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class MemberService {}
+export class MemberService {
+  constructor(@InjectModel
+    ("Member") private readonly memberModel: Model<null>
+  ) {}
+
+  public async signup(): Promise<String> {
+    return 'Signup executed!';
+  }
+
+  public async login(): Promise<String> {
+    return 'login executed!';
+  }
+
+  public async updateMember(): Promise<String> {
+    return 'updateMember executed!';
+  }
+
+  public async getMember(): Promise<String> {
+    return 'getMember executed!';
+  }
+
+  public async getMemberDetail(): Promise<String> {
+    return 'getMemberDetail executed!';
+  }
+}
