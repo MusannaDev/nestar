794733a210ce303254e9a2833464f9637d3dfb98
MusannaDev <juraevotabek2002@gmail.com>
2026-01-04 05:24:50 +0900

    feat: modify view module to record visits

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -11,6 +11,7 @@ import { RolesGuard } from '../auth/guards/roles.guard';
 import { MemberType } from '../../libs/enums/member.enum';
 import { MemberUpdate } from '../../libs/dto/member/member.update';
 import { shapeIntoMongoObjectId } from '../../libs/config';
+import { WithoutGuard } from '../auth/guards/without.guard';
 
 
 @Resolver()
@@ -58,11 +59,12 @@ export class MemberResolver {
     return this.memberService.updateMember(memberId, input);
   }
 
-  @Mutation(() => Member)
-  public async getMember(@Args('memberId') input: string): Promise<String> {
-    console.log("Mutation: getMember");
+  @UseGuards(WithoutGuard)
+  @Query(() => Member)
+  public async getMember(@Args('memberId') input: string, @AuthMember('_id') memberId: ObjectId): Promise<Member> {
+    console.log("Query: getMember");
     const targetId = shapeIntoMongoObjectId(input)
-    return this.memberService.getMember(targetId);
+    return this.memberService.getMember(memberId ,targetId);
   }
 
   /* ADMIN */
---
+++ apps/nestar-api/src/libs/dto/view/view.input.ts
@@ -0,0 +1,20 @@
+import { Field, InputType, } from "@nestjs/graphql";
+import type { ObjectId } from "mongoose";
+import { ViewGroup } from "../../enums/view.enum";
+import { IsNotEmpty } from "class-validator";
+
+@InputType()
+export class ViewInput {
+  @IsNotEmpty()
+  @Field(() => ViewGroup)
+  viewGroup: ViewGroup;
+
+  @IsNotEmpty()
+  @Field(() => String)
+  viewRefId: ObjectId;
+  
+  @IsNotEmpty()
+  @Field(() => String)
+  memberId: ObjectId;
+
+}
