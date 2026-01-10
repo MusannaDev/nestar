f632f541e975c115abb07a7db29e0be09580c6ba
MusannaDev <juraevotabek2002@gmail.com>
2026-01-10 18:54:27 +0900

    feat: develop updateProperty business logic

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -10,6 +10,8 @@ import { PropertyStatus } from '../../libs/enums/property.enum';
 import { ViewInput } from '../../libs/dto/view/view.input';
 import { ViewService } from '../view/view.service';
 import { ViewGroup } from '../../libs/enums/view.enum';
+import { PropertyUpdate } from '../../libs/dto/property/property.update';
+import moment from 'moment';
 
 @Injectable()
 export class PropertyService {
@@ -39,41 +41,73 @@ export class PropertyService {
 
 
   public async getProperty(memberId: ObjectId ,propertyId: ObjectId): Promise<Property> {
-      const search: T = {
-        _id: propertyId,
-        propertyStatus: PropertyStatus.ACTIVE,
-      };
-      const targetProperty: Property = await this.propertyModel.findOne(search).lean().exec();
-      if(!targetProperty) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
-  
-      if(memberId) {
-        //record view
-        const viewInput = { memberId: memberId, viewRefId: propertyId, viewGroup: ViewGroup.PROPERTY }
-        const newView = await this.viewService.recordView(viewInput)
-        if (newView) {
-          // increase memberView
-          await this.propertyStatsEditor({ _id: propertyId, targetKey: 'propertyViews', modifier: 1 });
-          targetProperty.propertyViews++
-        }
+    const search: T = {
+      _id: propertyId,
+      propertyStatus: PropertyStatus.ACTIVE,
+    };
+    const targetProperty: Property = await this.propertyModel.findOne(search).lean().exec();
+    if(!targetProperty) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
 
-        // Me Liked
-        
+    if(memberId) {
+      //record view
+      const viewInput = { memberId: memberId, viewRefId: propertyId, viewGroup: ViewGroup.PROPERTY }
+      const newView = await this.viewService.recordView(viewInput)
+      if (newView) {
+        // increase memberView
+        await this.propertyStatsEditor({ _id: propertyId, targetKey: 'propertyViews', modifier: 1 });
+        targetProperty.propertyViews++
       }
 
-      targetProperty.memberData = await this.memberService.getMember(null, targetProperty.memberId);
-      return targetProperty;
+      // Me Liked
+      
     }
 
+    targetProperty.memberData = await this.memberService.getMember(null, targetProperty.memberId);
+    return targetProperty;
+
+ }
+
     public async propertyStatsEditor(input: StatisticModifier): Promise<Property> {
-        const { _id, targetKey, modifier } = input;
-        console.log('executed');
-        return await this.propertyModel.findByIdAndUpdate(
-          _id,
-          { $inc: { [targetKey]: modifier } },
-          { new: true, },
-        )
+      const { _id, targetKey, modifier } = input;
+      console.log('executed');
+      return await this.propertyModel.findByIdAndUpdate(
+        _id,
+        { $inc: { [targetKey]: modifier } },
+        { new: true, },
+      )
+      .exec();
+    }
+
+
+    public async updateProperty(memberId: ObjectId, input: PropertyUpdate): Promise<Property> {
+      let { propertyStatus, soldAt, deletedAt } = input;
+      const search: T = {
+        _id: input._id,
+        memberId: memberId,
+        propertyStatus: PropertyStatus.ACTIVE,
+      };
+
+      if (propertyStatus === PropertyStatus.SOLD) soldAt = moment().toDate();
+      else if (propertyStatus === PropertyStatus.DELETE) deletedAt = moment().toDate();
+
+      const result = await this.propertyModel
+        .findOneAndUpdate(search, input, {
+          new: true,
+        })
         .exec();
+      if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+
+      if (soldAt || deletedAt) {
+        await this.memberService.memberStatsEditor({
+          _id: memberId,
+          targetKey: 'memberProperties',
+          modifier: -1,
+        });
       }
+
+      return result;
+    }
+  
   
 
 }
