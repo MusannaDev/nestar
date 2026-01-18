dacb19b6177e4b120bc32d13be31e4d71d0578d1
MusannaDev <juraevotabek2002@gmail.com>
2026-01-18 21:09:17 +0900

    feat: create getFavoriteProperties GraphQL API

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -1,7 +1,7 @@
 import { BadGatewayException, BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, ObjectId } from 'mongoose';
-import { AgentPropertiesInquiry, AllPropertiesInquiry, PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
+import { AgentPropertiesInquiry, AllPropertiesInquiry, OrdinaryInquiry, PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
 import { Properties, Property } from '../../libs/dto/property/property';
 import { Direction, Message } from '../../libs/enums/common.enum';
 import { MemberService } from '../member/member.service';
@@ -166,6 +166,14 @@ export class PropertyService {
       });
     }
   }
+
+
+  public async getFavorites(
+    memberId: ObjectId, input: OrdinaryInquiry
+  ): Promise<Properties> {
+    return await this.likeService.getFavoriteProperties(memberId, input);
+  }
+
 
 
   public async getAgentProperties(memberId: ObjectId, input: AgentPropertiesInquiry): Promise<Properties> {
