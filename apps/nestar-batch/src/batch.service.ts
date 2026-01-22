96c831fbbc22076ec98cf25f6a324367e0ace6f4
MusannaDev <juraevotabek2002@gmail.com>
2026-01-23 02:10:38 +0900

    feat: create batchRollback batchTopProperties batchTopAgents job schedules

--- apps/nestar-batch/src/batch.service.ts
+++ apps/nestar-batch/src/batch.service.ts
@@ -2,6 +2,19 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class BatchService {
+
+  public async batchRollback(): Promise<void> {
+    console.log("batchRollback");
+  }
+
+  public async batchTopProperties(): Promise<void> {
+    console.log("batchTopProperties");
+  }
+
+  public async batchTopAgents(): Promise<void> {
+    console.log("batchTopAgents")
+  }
+
   getHello(): string {
     return 'Welcome to Nestar Batch Server!';
   }
